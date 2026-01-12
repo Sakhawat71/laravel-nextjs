05eaf104c531ec0b42e91254238e1e85df3fef72
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2026-01-12 14:35:25 +0600

    login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <div className="text-center"></div>
+        <div className="flex min-h-screen items-center justify-center  font-sans">
+            <div className="text-center">
+                <h1>simple crud app</h1>
+            </div>
         </div>
     );
-}
+};
