27907161ea7b191ab02f2f92832e626aa87efed0
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2026-01-13 11:58:07 +0600

    single user by id

--- services/users/index.ts
+++ services/users/index.ts
@@ -14,3 +14,18 @@ export const getUsers = async () => {
         console.log(error);
     };
 };
+
+export const getUserById = async (id: string) => {
+    try {
+        const res = await fetch(`${process.env.API_URL}/users/${id}`,{
+            method: 'GET',
+            cache: 'no-store',
+        });
+        if (!res.ok) {
+            throw new Error("Failed to fetch blogs");
+        }
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    };
+};
